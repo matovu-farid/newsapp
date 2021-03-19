30427855821ab3f402245c28a5025a3088117bc5
matovu-farid <64433642+matovu-farid@users.noreply.github.com>
2021-03-19 22:54:01 +0300

    added auth

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,37 @@ import 'package:articlemodel/articlemodel.dart';
 import 'package:firebase_wrapper/firebase_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_lorem/flutter_lorem.dart';
+import 'package:lit_firebase_auth/lit_firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'created_widgets/pages/search.dart';
 import 'created_widgets/pages/timeline.dart';
-
+import 'package:unathenticated_widget/unathenticated_widget.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(FirebaseWrapper(child:
-  MultiProvider(
-    providers: [
-      ChangeNotifierProvider<WritersModel>(create: (_)=>WritersModel(),),
-      ChangeNotifierProvider(create: (_)=>ViewModel())
-    ],
-      child: MyApp())));
+  runApp(FirebaseWrapper(
+      child: MultiProvider(providers: [
+    ChangeNotifierProvider<WritersModel>(
+      create: (_) => WritersModel(),
+    ),
+    ChangeNotifierProvider(create: (_) => ViewModel())
+  ], child: MyApp())));
 }
-
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
-      title: 'Reader App',
-      theme: ThemeData(
-
-        primarySwatch: Colors.orange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return LitAuthInit(
+      authProviders: AuthProviders(google: true, emailAndPassword: false),
+      child: MaterialApp(
+        title: 'Reader App',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MyHomePage(title: 'Reader App'),
       ),
-      home: MyHomePage(title: 'Reader App'),
     );
   }
 }
@@ -46,41 +47,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-List<String> listOfTitles = List.generate(10, (index) => lorem(paragraphs: 1,words: 3));
-List<String> listOfSubtitles = List.generate(10, (index) => lorem(paragraphs: 1,words: 2));
-List<String> listOfContent = List.generate(10, (index) => lorem(paragraphs: 3,words: 600));
-GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-delete(int index){
-  setState(() {
-    listOfTitles.removeAt(index);
-    listOfContent.removeAt(index);
-    listOfSubtitles.removeAt(index);
-  });
-
-}
+  List<String> listOfTitles =
+      List.generate(10, (index) => lorem(paragraphs: 1, words: 3));
+  List<String> listOfSubtitles =
+      List.generate(10, (index) => lorem(paragraphs: 1, words: 2));
+  List<String> listOfContent =
+      List.generate(10, (index) => lorem(paragraphs: 3, words: 600));
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  delete(int index) {
+    setState(() {
+      listOfTitles.removeAt(index);
+      listOfContent.removeAt(index);
+      listOfSubtitles.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         key: scaffoldKey,
         appBar: AppBar(
-          title: Center(child: Text(widget.title)),
-          bottom: TabBar(tabs: [Tab(text:'Search'),Tab(text: 'TimeLine',)]),
+          title: Text(widget.title),
+          bottom: TabBar(tabs: [
+            Tab(text: 'Search'),
+            Tab(
+              text: 'TimeLine',
+            )
+          ]),
         ),
-        body: TabBarView(children: [
-          Search(),
-          TimeLine()
-        ]),
+        body: LitAuthState(
+
+          unauthenticated: Unauthenticated(),
+            authenticated: TabBarView(children: [Search(), TimeLine()])),
         // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
-
-
 }
-
